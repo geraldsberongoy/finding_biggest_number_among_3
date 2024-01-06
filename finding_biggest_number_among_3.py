69e2b9d6ccb22d81c993868652bf966a77a652d4
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-06 17:48:53 +0800

    If statement for first number

--- finding_biggest_number_among_3.py
+++ finding_biggest_number_among_3.py
@@ -8,10 +8,12 @@
 first_number = float(input("Enter first number: "))
 
 # Ask user to input number 2
-second_number = float(input("Enter second number"))
+second_number = float(input("Enter second number: "))
 
 # Ask user to input number 3
-third_number = float(input("Enter third number"))
+third_number = float(input("Enter third number: "))
 
 # Find highest among the inputted numbers using if-else statement
+if first_number > second_number > third_number:
+    print(first_number)
 # Diplay the highest number
