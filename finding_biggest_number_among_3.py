31cec52ebefdb556e4e928cffed23e4065869e37
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-06 20:14:00 +0800

    Using def function in if else statement

--- finding_biggest_number_among_3.py
+++ finding_biggest_number_among_3.py
@@ -14,11 +14,13 @@ second_number = float(input("Enter second number: "))
 third_number = float(input("Enter third number: "))
 
 # Find highest among the inputted numbers using if-else statement
-if first_number > second_number and first_number > third_number:
-    highest = first_number
-elif second_number > first_number and second_number> third_number:
-    highest = second_number
-else:
-    highest = third_number
+def highest_number(first_number, second_number, third_number):
+    if first_number > second_number and first_number > third_number:
+        return first_number
+    elif second_number > first_number and second_number> third_number:
+        return second_number
+    else:
+        return third_number
+
 # Diplay the highest number
-print(f"The highest number is: {highest:.2f}")
+print(f"The highest number is{highest_number(first_number, second_number, third_number):.2f}")
