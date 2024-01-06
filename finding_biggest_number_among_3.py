9ee8421408b00b357aa2fe3e6dcc45f39e6a366b
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-06 17:32:44 +0800

    Adding pseudocode

--- finding_biggest_number_among_3.py
+++ finding_biggest_number_among_3.py
@@ -3,4 +3,9 @@
 # Ask user to input 3 numbers. Find and print the 
 # biggest number using only if-else statement
 
-
+# pseudocode
+# Ask user to input number 1
+# Ask user to input number 2
+# Ask user to input number 3
+# Find highest among the inputted numbers using if-else statement
+# Diplay the highest number
