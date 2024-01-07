d6b0bd82b582d2099e08448c89a591929ce5ad68
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-07 13:25:07 +0800

    Making the pseudocode into comment and transforming it based onthe GUI ctkinter

--- finding_biggest_number_among_3.py
+++ finding_biggest_number_among_3.py
@@ -8,7 +8,6 @@ import customtkinter
 from tkinter import *
 from PIL import Image
 
-
 customtkinter.set_appearance_mode("dark")
 
 window = customtkinter.CTk()
@@ -22,6 +21,7 @@ def determine():
     second_number = float(entry_second_number.get())
     third_number = float(entry_third_number.get())
     print (first_number, second_number, third_number)
+    
 def highest_of_three(first_number, second_number, third_number):
     if first_number >= second_number and first_number >= third_number:
         return first_number
@@ -29,27 +29,33 @@ def highest_of_three(first_number, second_number, third_number):
         return second_number
     else:
         return third_number
+        
 # GUI WIDGETS
 
 banner_image = customtkinter.CTkImage(Image.open("banner.png"), size=(320, 100))
 label_banner_image = customtkinter.CTkLabel(window, text="", image=banner_image, corner_radius=100)
 label_banner_image.place(relx=0.5, rely=0.13, anchor=CENTER)
 
+# Ask user to input number 1
 entry_first_number = customtkinter.CTkEntry(window, placeholder_text="Enter the first number", placeholder_text_color="white"	, corner_radius=10, width=300, height=30)
 entry_first_number.place(relx=0.5, rely=0.3, anchor=CENTER)
 
+# Ask user to input number 2
 entry_second_number = customtkinter.CTkEntry(window, placeholder_text="Enter the second number",placeholder_text_color="white", corner_radius=10, width=300, height=30)
 entry_second_number.place(relx=0.5, rely=0.4, anchor=CENTER)
 
+# Ask user to input number 3
 entry_third_number = customtkinter.CTkEntry(window, placeholder_text="Enter the third number",placeholder_text_color="white", corner_radius=10, width=300, height=30)
 entry_third_number.place(relx=0.5, rely=0.5, anchor=CENTER)
 
+ # Function inside the command button that finds the highest among the inputted numbers using if-else statement
 determine_button = customtkinter.CTkButton(window, text="Determine", text_color="black", font=('Open Sans', 20, 'bold'), command= determine, fg_color="#E1A3FF", width=300, height=30)
 determine_button.place(relx=0.5, rely=0.6, anchor=CENTER)
 
 frame_result = customtkinter.CTkFrame(window, border_width=2, border_color="black", width=320, height=140)
 frame_result.place(relx=0.5, rely=0.81, anchor=CENTER)
-                   
+
+# Diplay the highest number                  
 result_label = customtkinter.CTkLabel(frame_result, text="Result", font=('Helvetica', 26, 'bold'))
 result_label.place(relx=0.5, rely=0.12, anchor=CENTER)
 
@@ -59,20 +65,20 @@ number_text_label.place(relx=0.5, rely=0.4, anchor=CENTER)
 highest_number_label = customtkinter.CTkLabel(frame_result, text="")
 highest_number_label.place(relx=0.5, rely=0.65, anchor=CENTER)
 
-# pseudocode
-# Ask user to input number 1
-first_number = float(input("Enter first number: "))
+# # pseudocode
+# # Ask user to input number 1
+# first_number = float(input("Enter first number: "))
 
-# Ask user to input number 2
-second_number = float(input("Enter second number: "))
+# # Ask user to input number 2
+# second_number = float(input("Enter second number: "))
 
-# Ask user to input number 3
-third_number = float(input("Enter third number: "))
+# # Ask user to input number 3
+# third_number = float(input("Enter third number: "))
 
-# Function that finds the highest among the inputted numbers using if-else statement
-highest_number = highest_of_three(first_number, second_number, third_number)
+# # Function that finds the highest among the inputted numbers using if-else statement
+# highest_number = highest_of_three(first_number, second_number, third_number)
 
-# Diplay the highest number
-print(f"The highest number is {highest_number:.2f}")
+# # Diplay the highest number
+# print(f"The highest number is {highest_number:.2f}")
 
 window.mainloop()
